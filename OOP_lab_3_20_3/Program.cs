e5094c70d130a213344913d76a439756ccb1e114
Роман Данилевич <dwordchanel@gmail.com>
2020-05-26 16:47:35 +0300

    Add project files.

---
+++ OOP_lab_3_20_3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_20_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("input.txt");
+            StreamWriter toFile = File.CreateText("output.txt");
+
+            string sentence = fromFile.ReadToEnd();
+
+            string[] words = sentence.Split(new char[] { (char)(92), '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int index = 0;
+
+            foreach (string word in words)
+            {
+                int k = 0;
+
+                for (int i = 0; i < word.Length; ++i)
+                {
+                    if (((word[i] >= (char)65) && (word[i] <= (char)90)) || ((word[i] >= (char)97) && (word[i] <= (char)122)))
+                    {
+                        ++k;
+                    }
+                }
+
+                if (word.Length != k)
+                {
+                    words[index] = words[index].Remove(0);
+                }
+
+                ++index;
+            }
+
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    toFile.Write("{0} ", word);
+                }
+            }
+
+            toFile.Close();
+            fromFile.Close();
+        }
+    }
+}
